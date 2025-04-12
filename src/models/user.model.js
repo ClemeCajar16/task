d7da5f94206a49e5aabc2b466a6c34881d7ca0d9
John Doe <ccajarlaguna@gmail.com>
2025-04-12 10:52:18 -0500

    ruta

---
+++ src/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+
+    username:{
+        String,
+        required:true,
+        trim: true
+    },
+    email: {
+        String,
+        required:true,
+        trim: true,
+        unique: true
+    },
+    password:{
+        String,
+        required:true
+    }
+});
+
+export default mongoose.model('User', userSchema);
