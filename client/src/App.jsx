751653ef944a2e1dda6bc0f4ee640101326ecdda
Cleme Cajar <ccajarlaguna@gmail.com>
2025-05-06 11:28:51 -0500

    configuración del frontend con vite-react

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+
+  return (
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<h1>login</h1>} />
+        <Route path="/register" element={<h1>register</h1>} />
+        <Route path="/taks" element={<h1>tasks page</h1>} />
+        <Route path="/add-task" element={<h1>new task</h1>} /> 
+        <Route path="/taks/:id" element={<h1>update task</h1>} />
+        <Route path="/profile" element={<h1>profile</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
