86a156a363fb5b5264e5800127b599725f7d1a40
Cleme Cajar <ccajarlaguna@gmail.com>
2025-05-08 12:09:10 -0500

    registro de usuarios ;-)

--- frontend/client/src/context/AuthContext.jsx
+++ frontend/client/src/context/AuthContext.jsx
@@ -13,15 +13,23 @@ export const useAuth = () => {
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   const signup = async (user) => {
+   try {
+    
     const res = await registerRequest(user);
     console.log(res.data);
     setUser(res.data);
+    setIsAuthenticated(true);
+
+   } catch (error) {
+    console.log(error);
+   }
   };
 
   return (
-    <AuthContext.Provider value={{ user, signup }}>
+    <AuthContext.Provider value={{ user, signup, isAuthenticated }}>
       {children}
     </AuthContext.Provider>
   );
--- frontend/client/src/pages/registerPage.jsx
+++ frontend/client/src/pages/registerPage.jsx
@@ -1,27 +1,35 @@
 import { useForm } from 'react-hook-form';
 import { useAuth } from '../context/AuthContext';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function RegisterPage() {
-    
-    const {register, handleSubmit} = useForm();
-const {signup, user} = useAuth();
 
-console.log(user);
+    const { register, handleSubmit } = useForm();
+    const { signup, isAuthenticated } = useAuth();
+    const navigation = useNavigate();
 
-    return(
+
+    useEffect(() => {
+if(isAuthenticated) {
+            navigation('/tasks')
+        }
+    }, [isAuthenticated])
+
+    return (
 
         <div className='bg-sky-300 max-w-md p-20 rounded-md '>
 
-            <form onSubmit={handleSubmit (async values => {
+            <form onSubmit={handleSubmit(async values => {
                 signup(values);
-             
+
             })}>
-                <input type="text" {...register("username", {required:true})} className='w-full  bg-zinc-700 text-white px-4 py-2 rounded-b-md my-3' placeholder='username'/>
-                <input type="email" {...register("email", {required:true})} className='w-full  bg-zinc-700 text-white px-4 py-2 rounded-b-md my-3'placeholder='email'/>
-                <input type="password" {...register("password", {required:true})} className='w-full  bg-zinc-700 text-white px-4 py-2 rounded-b-md my-3'placeholder='password' />
+                <input type="text" {...register("username", { required: true })} className='w-full  bg-zinc-700 text-white px-4 py-2 rounded-b-md my-3' placeholder='username' />
+                <input type="email" {...register("email", { required: true })} className='w-full  bg-zinc-700 text-white px-4 py-2 rounded-b-md my-3' placeholder='email' />
+                <input type="password" {...register("password", { required: true })} className='w-full  bg-zinc-700 text-white px-4 py-2 rounded-b-md my-3' placeholder='password' />
 
                 <button type='submit'>
-        register
+                    register
                 </button>
 
             </form>
