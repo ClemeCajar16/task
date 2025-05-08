e75b12e1418a0e86e98d0acb7aba2d208a3117aa
Cleme Cajar <ccajarlaguna@gmail.com>
2025-05-08 13:47:51 -0500

    creación de usuario ╰(*°▽°*)╯

--- frontend/client/src/pages/registerPage.jsx
+++ frontend/client/src/pages/registerPage.jsx
@@ -5,28 +5,37 @@ import { useNavigate } from 'react-router-dom';
 
 function RegisterPage() {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const { signup, isAuthenticated } = useAuth();
     const navigation = useNavigate();
 
 
     useEffect(() => {
-if(isAuthenticated) {
+        if (isAuthenticated) {
             navigation('/tasks')
         }
     }, [isAuthenticated])
 
     return (
 
-        <div className='bg-sky-300 max-w-md p-20 rounded-md '>
+        <div className='bg-zinc-800 max-w-md p-20 rounded-md'>
 
             <form onSubmit={handleSubmit(async values => {
                 signup(values);
 
             })}>
-                <input type="text" {...register("username", { required: true })} className='w-full  bg-zinc-700 text-white px-4 py-2 rounded-b-md my-3' placeholder='username' />
-                <input type="email" {...register("email", { required: true })} className='w-full  bg-zinc-700 text-white px-4 py-2 rounded-b-md my-3' placeholder='email' />
-                <input type="password" {...register("password", { required: true })} className='w-full  bg-zinc-700 text-white px-4 py-2 rounded-b-md my-3' placeholder='password' />
+
+
+
+                <input type="text" {...register("username", { required: true })} className='w-full  bg-zinc-400 text-black px-4 py-2 rounded-b-md my-3' placeholder='username' />
+                {errors.username && <p className='text-red-500'>username is required </p>}
+
+                <input type="email" {...register("email", { required: true })} className='w-full  bg-zinc-400 text-black px-4 py-2 rounded-b-md my-3' placeholder='email' />
+                {errors.email && <p className='text-red-500'>email is required </p>}
+
+                <input type="password" {...register("password", { required: true })} className='w-full  bg-zinc-400 text-black px-4 py-2 rounded-b-md my-3' placeholder='password' />
+                {errors.password && <p className='text-red-500'>password is required </p>}
+
 
                 <button type='submit'>
                     register
