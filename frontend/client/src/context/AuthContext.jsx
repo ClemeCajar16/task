276560cee03ad38e8874b5c3011cb9283813ba71
Cleme Cajar <ccajarlaguna@gmail.com>
2025-05-08 10:45:53 -0500

    creación de usuario ╰(*°▽°*)╯

--- frontend/client/src/context/AuthContext.jsx
+++ frontend/client/src/context/AuthContext.jsx
@@ -1,30 +1,28 @@
 import { createContext, useState, useContext } from "react";
 import { registerRequest } from "../api/auth.js";
 
-
 export const AuthContext = createContext();
-export const useAuth = () => {
-    const context = useContext(AuthContext);
 
-if (!context) {
-        throw new Error("useAuth must be used within an AuthProvider");
-        
-    }
-    return context;
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
 };
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    const singup = async (user) => {
-        const res = await registerRequest(values)
-        console.log(res.data);
-        setUser(res.data);
-    }
+  const signup = async (user) => {
+    const res = await registerRequest(user);
+    console.log(res.data);
+    setUser(res.data);
+  };
 
-    return (
-        <AuthContext.Provider value={{singup, user}}>
-            {children}
-        </AuthContext.Provider>
-    );
-};  
+  return (
+    <AuthContext.Provider value={{ user, signup }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- frontend/client/src/pages/registerPage.jsx
+++ frontend/client/src/pages/registerPage.jsx
@@ -1,15 +1,19 @@
 import { useForm } from 'react-hook-form';
-
+import { useAuth } from '../context/AuthContext';
 
 function RegisterPage() {
-
+    
     const {register, handleSubmit} = useForm();
+const {signup, user} = useAuth();
+
+console.log(user);
+
     return(
 
         <div className='bg-sky-300 max-w-md p-20 rounded-md '>
 
             <form onSubmit={handleSubmit (async values => {
-                console.log(values);
+                signup(values);
              
             })}>
                 <input type="text" {...register("username", {required:true})} className='w-full  bg-zinc-700 text-white px-4 py-2 rounded-b-md my-3' placeholder='username'/>
